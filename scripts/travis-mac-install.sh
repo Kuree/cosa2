47b62d16faff34e16be5f2b2546608a174361ed4
makaimann <makaim@stanford.edu>
2020-03-12 00:12:56 -0700

    Install flex with brew

--- scripts/travis-mac-install.sh
+++ scripts/travis-mac-install.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     export PATH=/usr/local/bin:/usr/local/sbin:$PATH
-    brew install gnu-getopt
+    brew install gnu-getopt flex
     brew upgrade flex
 else
     echo "NOT in OSX -- nothing to do"
