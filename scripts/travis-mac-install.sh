3d20c7c178bd30037c59ca6740cdaf57cbff6694
Makai Mann <makaim@stanford.edu>
2020-03-12 08:36:51 -0700

    Install newer flex and point to location

--- scripts/travis-mac-install.sh
+++ scripts/travis-mac-install.sh
@@ -3,8 +3,11 @@
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     export PATH=/usr/local/bin:/usr/local/sbin:$PATH
-    brew install gnu-getopt flex
+    brew install gnu-getopt gmp flex@2.6.4
     brew upgrade flex
+    export PATH="/usr/local/opt/flex/bin:$PATH"
+    export LDFLAGS="-L/usr/local/opt/flex/lib"
+    export CPPFLAGS="-I/usr/local/opt/flex/include"
 else
     echo "NOT in OSX -- nothing to do"
 fi
